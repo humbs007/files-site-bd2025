8292b50e917bee7e0c869d1a2b95016b33752d33
Humberto Gomes <humbs007@gmail.com>
2025-04-28 11:40:48 -0300

    28041140

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM python:3.11-slim-bullseye
+FROM python:3.11-slim
 
 WORKDIR /app
 
 COPY ./requirements.txt .
-RUN apt-get update && apt-get upgrade -y && pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY ./app ./app
 COPY ./main.py .
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -10,14 +10,27 @@ project_root = os.getenv('PYTHONPATH')
 if project_root and project_root not in sys.path:
     sys.path.append(project_root)
 
-# Agora sim, importa normalmente
+# Importações FastAPI
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import router
 
+# Inicializa FastAPI
 app = FastAPI(title="Buscador Multi Dados V2")
 
+# 🚨 Adiciona Middleware CORS aqui
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Liberado para qualquer origem - podemos depois restringir se quiser
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Inclui as rotas
 app.include_router(router, prefix="/api/v1")
 
+# Endpoint raiz
 @app.get("/")
 async def root():
     return {"message": "Bem-vindo ao Buscador Multi Dados V2 🚀"}
